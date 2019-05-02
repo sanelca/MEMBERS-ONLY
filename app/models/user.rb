98daa459fc2bf5a5c3c9e03a8eb3f8ccda32fc75
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-05-02 12:46:48 +0200

    Remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,41 +1,73 @@
+# class User < ActiveRecord::Base
+#   attr_accessor :remember_token, :activation_token
+#   before_create :create_activation_digest
+#   before_create :create_remember_token
+#   before_save { self.email = email.downcase }
+#   validates :username,  presence: true, length: { maximum: 50 }
+#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#   validates :email, presence: true, length: { maximum: 255 },
+#                     format: { with: VALID_EMAIL_REGEX },
+#                     uniqueness: { case_sensitive: false }
+#
+#   has_secure_password
+#   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+#   def User.digest(string)
+#     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+#                                                   BCrypt::Engine.cost
+#     BCrypt::Password.create(string, cost: cost)
+#   end
+#   def User.new_token
+#     SecureRandom.urlsafe_base64
+#   end
+#   def remember
+#     self.remember_token = User.new_token
+#     update_attribute(:remember_digest, User.digest(remember_token))
+#   end
+#   def authenticated?(remember_token)
+#     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+#   end
+#   def forget
+#     update_attribute(:remember_digest, nil)
+#   end
+#   private
+#     def create_activation_digest
+#       self.activation_token  = User.new_token
+#       self.activation_digest = User.digest(activation_token)
+#     end
+#     def create_remember_token
+#       self.remember_token = User.new_token
+#       update_attribute(:remember_token, User.digest(remember_token))
+#     end
+# end
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
-  before_create :create_activation_digest
-  before_create :create_remember_token
-  before_save { self.email = email.downcase }
-  validates :username,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  attr_accessor :remember_token
 
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
-  end
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
-  def forget
-    update_attribute(:remember_digest, nil)
-  end
-  private
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
-    def create_remember_token
-      self.remember_token = User.new_token
-      update_attribute(:remember_digest, User.digest(remember_token))
-    end
+	before_create { self.remember_token = digest(new_token) }
+
+	has_many :posts
+
+	validates :username, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+     validates :email, presence: true, length: { maximum: 255 },
+                       format: { with: VALID_EMAIL_REGEX },
+                       uniqueness: { case_sensitive: false }  
+	has_secure_password
+
+	def new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def digest(token)
+		Digest::SHA1.hexdigest(token)
+	end
+
+	def remember
+		self.remember_token = new_token
+		update_attribute(:remember_token, digest(remember_token))
+	end
+
+	def forget
+		update_attribute(:remember_token, nil)
+	end
 end
